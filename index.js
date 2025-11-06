06cf996b419e40872b30b61b14c9cc4e0d62720b
Martaarl <marali23@gmail.com>
2025-11-06 09:41:42 +0100

    obteniendo datos, que incluyan las diferentes páginas

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+
+const gamesArray = [];
+
 
 const scrapper = async (url) => {
 
@@ -12,12 +17,43 @@ const scrapper = async (url) => {
 
     const arrayDivs = await page.$$(".force-badge")//selecciona los divs que tienen esa clase y de ahí saco todos los juegos
     
+   
+
     for (const gameDiv of arrayDivs) {
-        const className = await gameDiv.evaluate((el) => el.className);
+        let price;
+        let title = await gameDiv.$eval(".title", (el) => el.textContent);
+       // console.log(title)
+        let img =  await gameDiv.$eval("img", (el) => el.src);
+       // console.log(img);
+    
+        //evaluate((el) => el.className); esta sería la forma de añadir solo el className detrás del await si no utilizamos el eval
+        try {
+            price = await gameDiv.$eval(".price", (el) => 
+                parseFloat(el.textContent.slice(0, el.textContent.length -1)));
+            
+        } catch (error) {
+            price = "0";
+        }
+      //  console.log(price);
 
+        const game = {
+        title, 
+        price,
+        img
     }
+     // console.log(game); 
+      gamesArray.push(game);
+    }
+    write(gamesArray); 
 
+    const arrow = await page.$eval("[title='Next']");
+   
+};
 
-}
+const write = (gamesArray) => {
+    fs.writeFile("games.json", JSON.stringify(gamesArray), () =>{
+        console.log("Archivo escrito");
+    });
+};
 
-scrapper("https://www.instant-gaming.com/es/pc/steam/");
+scrapper("https://www.instant-gaming.com/es/pc/steam/tendencias/");
